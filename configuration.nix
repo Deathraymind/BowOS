6e85e62a3630ff071f8cdf66679533248745ee2c
Deathraymind <deathraymind@gmail.com>
2024-08-06 20:38:24 +0200

    formating

--- configuration.nix
+++ configuration.nix
@@ -70,12 +70,14 @@ nixpkgs.config.packageOverrides = pkgs: {
     packages = with pkgs; [];
   };
 
-#,------.                ,--.     ,--.                 
-#|  .-.  \  ,---.  ,---. |  |,-.,-'  '-. ,---.  ,---.  
-#|  |  \  :| .-. :(  .-' |     /'-.  .-'| .-. || .-. | 
-#|  '--'  /\   --..-'  `)|  \  \  |  |  ' '-' '| '-' ' 
-#`-------'  `----'`----' `--'`--' `--'   `---' |  |-'  
-#                                             `--'    
+
+
+# ____            _    _              
+#|  _ \  ___  ___| | _| |_ ___  _ __  
+#| | | |/ _ \/ __| |/ / __/ _ \| '_ \ 
+#| |_| |  __/\__ \   <| || (_) | |_) |
+#|____/ \___||___/_|\_\\__\___/| .__/ 
+#                              |_|      
 
   programs.hyprland = {
     # Install the packages from nixpkgs
@@ -84,18 +86,16 @@ nixpkgs.config.packageOverrides = pkgs: {
     xwayland.enable = true;
   };
 
+#__            _                         
+#|  _ \ __ _  ___| | ____ _  __ _  ___  ___ 
+#| |_) / _` |/ __| |/ / _` |/ _` |/ _ \/ __|
+#|  __/ (_| | (__|   < (_| | (_| |  __/\__ \
+#|_|   \__,_|\___|_|\_\__,_|\__, |\___||___/
+#                           |___/           
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  
-#,------.               ,--.                                 
-#|  .--. ' ,--,--. ,---.|  |,-. ,--,--. ,---.  ,---.  ,---.  
-#|  '--' |' ,-.  || .--'|     /' ,-.  || .-. || .-. :(  .-'  
-#|  | --' \ '-'  |\ `--.|  \  \\ '-'  |' '-' '\   --..-'  `) 
-#`--'      `--`--' `---'`--'`--'`--`--'.`-  /  `----'`----'  
-#                                     `---'          
-  
   environment.systemPackages = with pkgs; [
     git
     kitty
@@ -201,41 +201,69 @@ nixpkgs.config.packageOverrides = pkgs: {
   ];
 
 
+
   fonts.fonts = with pkgs; [
      nerdfonts # this pulls the nerdfonts from the nixos and makes it available. 
   ];
 
-#,---.                          ,--.                     
-#'   .-'  ,---. ,--.--.,--.  ,--.`--' ,---. ,---.  ,---.  
-#`.  `-. | .-. :|  .--' \  `'  / ,--.| .--'| .-. :(  .-'  
-#.-'    |\   --.|  |     \    /  |  |\ `--.\   --..-'  `) 
-#`-----'  `----'`--'      `--'   `--' `---' `----'`----' 
-
-
-
+# ____                  _      _           
+#/ ___|  ___ _ ____   _(_) ___(_) ___  ___ 
+#\___ \ / _ \ '__\ \ / / |/ __| |/ _ \/ __|
+# ___) |  __/ |   \ V /| | (__| |  __/\__ \
+#|____/ \___|_|    \_/ |_|\___|_|\___||___/
 
   programs.kdeconnect.enable = true; 
-
-  
   
   services.udisks2.enable = true; 
-
   security.polkit.enable = true;
-  
   services.expressvpn.enable = true;
-  
   services.flatpak.enable = true;
-
-  
-
   services.openssh.enable = true; # enables the sshd server on the computer
+  services.vscode-server.enable = true; # this enables the vs code server.
+  
   services.openssh.permitRootLogin = "yes";  # // or "no" if you want to disable root login
   services.openssh.passwordAuthentication = true; # // or false to disable password authentication
-  
-  services.vscode-server.enable = true; # this enables the vs code server. 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # this is a nixos experimental feature called flakes
 
- networking.firewall = {
+  # Steam
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  }; 
+ 
+  # Graphics
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  # Enable networking
+  networking.networkmanager.enable = true; # Enables dhcp and ethernet support IMPORTANT
+
+  # Bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
+  # Sound
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = false;
+  };
+
+# _____ _                        _ _ 
+#|  ___(_)_ __ _____      ____ _| | |
+#| |_  | | '__/ _ \ \ /\ / / _` | | |
+#|  _| | | | |  __/\ V  V / (_| | | |
+#|_|   |_|_|  \___| \_/\_/ \__,_|_|_|
+
+networking.firewall = {
   enable = true; # Make sure the firewall is enabled
   
   allowedTCPPorts = [ 9943 9944 51112 ]; # List of TCP ports to open
@@ -251,66 +279,11 @@ nixpkgs.config.packageOverrides = pkgs: {
 virtualisation.libvirtd.enable = true;
 programs.virt-manager.enable = true;
 
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = false;
-  # If you want to use JACK applications, uncomment this
-  # systemctl --user restart pipewire.service
-  # systemctl --user restart pipewire-pulse.service
-  };
-
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
-
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  }; 
- 
-hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-services.xserver.videoDrivers = ["amdgpu"];
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-
   
   nixpkgs.config.permittedInsecurePackages = [
  "electron-25.9.0"
   ];
-
-
-
-  # Enable networking
-  networking.networkmanager.enable = true; # Enables dhcp and ethernet support IMPORTANT
-
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # this is a nixos experimental feature called flakes
   system.stateVersion = "24.05"; # Did you read the comment? tahdah
 }
