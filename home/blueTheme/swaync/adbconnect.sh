edbdde4baf940316b1a26b7667ba28260b4d5423
Deathraymind <deathraymind@gmail.com>
2024-08-18 00:36:16 +0200

    tcp connect for adb

--- home/blueTheme/swaync/adbconnect.sh
+++ home/blueTheme/swaync/adbconnect.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Prompt the user to enable wireless debugging on their Android phone
-echo "Please open up Developer Options and enable Wireless Debugging on your Android phone."
-echo "Look for the 'Pair with code' option. Ensure both this computer and the phone are on the same network!"
-
-# Ask for the IP address
+# Prompt the user to enable wireless debugging and find the IP and port
+echo "Please go to the Wireless Debugging options on your Android phone and locate the IP address and port."
 echo -n "Please enter the IP address: "
 read ip
 
 # Clear the screen
 clear
 
-# Ask for the port number
+# Prompt the user for the port number
 echo -n "Please enter the port number: "
 read port
 
 # Clear the screen
 clear
 
-# Pair the device using the provided IP and port
-adb pair "$ip:$port"
+# Attempt to connect using scrcpy over TCP/IP
+scrcpy --tcpip="$ip:$port"
