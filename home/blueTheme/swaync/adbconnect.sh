da741dc08a92de1bbf99af6505beceb6926f65a9
Deathraymind <deathraymind@gmail.com>
2024-08-18 00:28:12 +0200

    adb script

---
+++ home/blueTheme/swaync/adbconnect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please open up developer wireless debuging on youre android phone and look for pair with code. Be sure both this computer and phone are on the same network!"
+echo "Please enter the IP"
+read ip
+clear
+echo "Please enter port"
+read port
+clear
+
+adb pair $ip:$port
