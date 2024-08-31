0ef9bfa05e30b8702cfcdfb775cd32193c186aa8
Deathraymind <deathraymind@gmail.com>
2024-08-31 02:46:12 +0200

    treesitter

--- home/blueTheme/nvim/init.lua
+++ home/blueTheme/nvim/init.lua
@@ -16,17 +16,6 @@ local opts = {}
 vim.g.mapleader = " "
 require("lazy").setup("plugins")
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = {enable = true},
-  indent = {enable = true},
-})
--- home/blueTheme/nvim/lua/plugins.lua
+++ home/blueTheme/nvim/lua/plugins.lua
@@ -1,8 +1,8 @@
 
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    
+    
   
     {
       "nvim-neo-tree/neo-tree.nvim",
