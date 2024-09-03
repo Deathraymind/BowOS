0e245582dfd615e847ebad8162c51df6164f7110
Deathraymind <deathraymind@gmail.com>
2024-09-03 05:57:09 +0200

    lazy fix

--- home/blueTheme/nvim/init.lua
+++ home/blueTheme/nvim/init.lua
@@ -1,20 +1,17 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- Install lazy.nvim if not already installed
+require("config.lazy")
 
-local opts = {}
+
+
+
+
+-- lazy.setup("plugins")  -- Ensure "plugins.lua" exists in your config directory
+
+-- Set leader key
 vim.g.mapleader = " "
-require("lazy").setup("plugins")
 
 -- Map yy to yank to both the default register and the system clipboard
 vim.api.nvim_set_keymap('n', 'yy', 'yy"+yy', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-x>', ':lcd %:p:h<CR>:ToggleTerm<CR>', { noremap = true, silent = true }) --This is for toggleterm ctrl x will open temrinal in current directory
+
+-- Map Ctrl-x to open terminal in the current directory
+vim.api.nvim_set_keymap('n', '<C-x>', ':lcd %:p:h<CR>:ToggleTerm<CR>', { noremap = true, silent = true })
