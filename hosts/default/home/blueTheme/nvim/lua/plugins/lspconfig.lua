34db9235b03dc30a61d88f3e552b7e1ea8fbef5d
Deathraymind <deathraymind@gmail.com>
2024-09-08 13:35:56 +0900

    auto completions

--- hosts/default/home/blueTheme/nvim/lua/plugins/lspconfig.lua
+++ hosts/default/home/blueTheme/nvim/lua/plugins/lspconfig.lua
@@ -3,10 +3,14 @@ return {
 'neovim/nvim-lspconfig',
   config = function()
     local lspconfig = require('lspconfig')
+    local capabilities = require('cmp_nvim_lsp').default_capabilities() -- This is needed for settiing up auto completions by reaching out to the lsp servers
 
-
-    lspconfig.nixd.setup{}
-    lspconfig.lua_ls.setup{}
+    lspconfig.nixd.setup({
+        capabilities = capabilities
+        })
+    lspconfig.lua_ls.setup({
+        capabilities = capabilities
+        })
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
