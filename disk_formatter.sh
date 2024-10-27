95480b962240330415a0afc78b439c964e2357df
Deathraymind <deathraymind@gmail.com>
2024-10-27 10:32:22 +0900

    cmon work

--- disk_formatter.sh
+++ disk_formatter.sh
@@ -1,48 +1,52 @@
-#!/bin/bash                                                                                              
+#!/bin/bash
+
 # Redirect all output to a log file
 exec > >(tee -i /mnt/disk_formatter.log)
 exec 2>&1
 
-# Call the Python Textual UI to format the disk
+# Call the Python Textual UI to format the disk (Add the actual command here)
+# python3 format_disk.py
 
 # After the disk is formatted, continue with the NixOS installation
 nixos-generate-config --root /mnt
 nixos-install --no-root-passwd
-nixos-enter -- nix-shell -p git --run '
-git clone --branch beta https://github.com/deathraymind/BowOS
 
-cd BowOS
-rm -r .git 
-rm flake.lock
-nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-nix-channel --update
-export NIXPKGS_ALLOW_INSECURE=1
-export NIX_USER=bowyn
-export NIX_PASSWORD=6255
+# Enter NixOS environment and run further setup
+nixos-enter -- nix-shell -p git -p expect --run '
+  git clone --branch beta https://github.com/deathraymind/BowOS
 
-passwd useradd -m NIX_USER
+  cd BowOS
+  rm -r .git 
+  rm flake.lock
+  nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+  nix-channel --update
+  export NIXPKGS_ALLOW_INSECURE=1
+  export NIX_USER=bowyn
+  export NIX_PASSWORD=6255
 
-nix-shell -p expect --run "
-expect -c \"
-spawn passwd \$NIX_USER
-expect \\\"New password:\\\"
-send \\\"\$NIX_PASSWORD\\r\\\"
-expect \\\"Retype new password:\\\"
-send \\\"\$NIX_PASSWORD\\r\\\"
-expect eof
-\"
+  # Create the user
+  useradd -m "$NIX_USER"
 
-expect -c \"
-spawn passwd root
-expect \\\"New password:\\\"
-send \\\"\$NIX_PASSWORD\\r\\\"
-expect \\\"Retype new password:\\\"
-send \\\"\$NIX_PASSWORD\\r\\\"
-expect eof
-\"
-"
+  # Set the password for the new user and root using expect
+  expect -c "
+    spawn passwd $NIX_USER
+    expect \"New password:\"
+    send \"$NIX_PASSWORD\r\"
+    expect \"Retype new password:\"
+    send \"$NIX_PASSWORD\r\"
+    expect eof
+  "
 
+  expect -c "
+    spawn passwd root
+    expect \"New password:\"
+    send \"$NIX_PASSWORD\r\"
+    expect \"Retype new password:\"
+    send \"$NIX_PASSWORD\r\"
+    expect eof
+  "
 
-nixos-rebuild boot --install-bootloader --impure --flake .
+  # Rebuild the system with the new configurations
+  nixos-rebuild boot --install-bootloader --impure --flake .
 '
 
