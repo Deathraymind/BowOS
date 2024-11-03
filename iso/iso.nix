c7e7f212a77a74d3fc4a599f723784b51aea6f7e
Deathraymind <deathraymind@gmail.com>
2024-11-04 08:17:57 +0900

    stuff

--- iso/iso.nix
+++ iso/iso.nix
@@ -7,6 +7,7 @@
     # to creat a .nar file with all the goofy files use this 
     # nix-store -qR /run/current-system > installed-packages.txt 
     # nix-store --export $(cat installed-packages.txt) > all-installed-packages.nar
+    # scp root@192.168.122.53:/mnt/all-installed-packages.nar /home/bowyn/BowOSv0.01/iso
 
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
@@ -18,67 +19,45 @@
       neovim
       git
       kitty
-      waybar
-      rofi
       ethtool
-      gnome.nautilus
-      wl-clipboard
       pciutils
       usbutils
-      pipewire
       pamixer
       networkmanager
       networkmanagerapplet
-      bluez
-      blueman
       brightnessctl
       playerctl
-      dunst
-      swaynotificationcenter
-      grim
-      slurp
-      swappy
-      cliphist
-      hyprpicker
-      polkit-kde-agent
-      xdg-desktop-portal-hyprland
       imagemagick
       ffmpegthumbs
       nwg-look
-      qt5ct
-      qt6ct
-      gtk4
-      gtk3
-      hyprland
-      hyprpaper
-      fastfetch
-      hyprlock
-      pavucontrol
       pipewire
-      xorg.xrandr
-      home-manager
       htop
-      playerctl
       dconf
       glib
-      gnome.gnome-disk-utility
-      udisks2
-      firefox
-      pipewire
-      wireplumber
-      scrcpy
-      android-tools
-      gcc
       cpufrequtils
       (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
       python3
       python3Packages.textual
 
     ];
+
+
+
+
+    environment = {
+  variables = {
+	http_proxy = "http://192.168.49.1:8000";
+	https_proxy = "http://192.168.49.1:8000";
+	ftp_proxy = "http://192.168.49.1:8000";
+	no_proxy = "localhost,127.0.0.1,::1";
+  };
+};
+
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerdfonts/shas.nix for the types of fonts
     isoImage.squashfsCompression = "gzip -Xcompression-level 1";
     environment.etc."install_bowos.sh".source = /home/bowyn/BowOSv0.01/iso/install_bowos.sh;
-
+    environment.etc."bowos-packages.nar".source =/home/bowyn/BowOSv0.01/iso/bowos-packages.nar;
+    environment.etc."prox.sh".source = /home/bowyn/BowOSv0.01/iso/prox.sh;
     
     services.openssh.enable = true; # enables the sshd server on the computer 
     services.openssh.permitRootLogin = "yes";  # // or "no" if you want to disable root login
