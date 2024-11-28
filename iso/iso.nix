27ac6d3bc9e58582b9db661063ac0c6d4ed3e013
bowyn Rosch <deathraymind@gmail.com>
2024-11-28 13:09:44 +0900

    removed proxy stuff

--- iso/iso.nix
+++ iso/iso.nix
@@ -7,7 +7,7 @@
     #  nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=./iso.nix
     # to creat a .nar file with all the goofy files use this 
     # nix-store -qR /run/current-system > installed-packages.txt 
-    # nix-store --export $(cat installed-packages.txt) > all-installed-packages.nar
+    # nix-store --export $(cat installed-packages.txt) > bowos-packages.nar
     # scp root@192.168.122.53:/mnt/all-installed-packages.nar /home/bowyn/BowOSv0.01/iso
     # nix-store --export $(nix-store -qR /run/current-system) > system-packages.nar
     # Provide an initial copy of the NixOS channel so that the user
@@ -47,14 +47,6 @@
 
 
 
-    environment = {
-  variables = {
-	http_proxy = "http://192.168.49.1:8000";
-	https_proxy = "http://192.168.49.1:8000";
-	ftp_proxy = "http://192.168.49.1:8000";
-	no_proxy = "localhost,127.0.0.1,::1";
-  };
-};
 
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerdfonts/shas.nix for the types of fonts
     isoImage.squashfsCompression = "gzip -Xcompression-level 1";
