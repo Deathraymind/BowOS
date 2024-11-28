155510c48d1836506a5bad0f32624b17164a591a
bowyn Rosch <deathraymind@gmail.com>
2024-11-28 21:00:20 +0900

    formated: stylix.nix

--- hosts/default/stylix.nix
+++ hosts/default/stylix.nix
@@ -1,23 +1,31 @@
-{ config, pkgs, inputs, lib, ... }: 
+# stylix.nix
+{ config, pkgs, inputs, lib, ... }: {
 
-{
+  stylix = {
+    enable = true;
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 32;
+    };
 
+    image = /home/bowyn/bowos/wallpaper/wallpapers/wp.png;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-stylix.enable=true;
-stylix.cursor.package = pkgs.bibata-cursors;
-stylix.image = /home/bowyn/bowos/wallpaper/wallpapers/wp.png;
-stylix.cursor.name = "Bibata-Modern-Ice";
-stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-stylix.cursor.size = 32;
-stylix.polarity = "dark";
-stylix.autoEnable = true;
-    # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-
-  stylix.fonts = {
-        monospace = {
-            package = pkgs.google-fonts;  # Replace with the correct package for Roboto Mono
-            name = "Roboto Mono";         # Specify the font name
-        };
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font";
+      };
+      serif = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font";
+      };
+    };
   };
-
 }
