a7dbfd233c566322a2846974c253dd67cbd1bc9a
bowyn Rosch <deathraymind@gmail.com>
2024-11-29 21:45:21 +0900

    hash for wallaper fixed

--- hosts/default/homeStylix.nix
+++ hosts/default/homeStylix.nix
@@ -5,7 +5,7 @@
     enable = true;
     image = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/orxngc/walls-catppuccin-mocha/master/dominik-mayer-17.jpg";
-      sha256 = "sha256-bEBcdjXbUBZdbgxIz2pBzOi+wp47m/siB+1XI7hOUHY=";
+      sha256 = "sha256-g/Bi1RfjMFxnFlu5ovok+vOkCCtZ8iC8Uf2fKabt9gA=";
     }; 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
