78b54f6cc7697c215fa6ce6e33e08d9077ffff9a
bowyn Rosch <deathraymind@gmail.com>
2025-03-19 09:33:57 +0900

    swaync

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -12,19 +12,20 @@ in
     ./home/blueTheme/fastfetch/fastfetch.nix
     ./home/blueTheme/hypr/hyprland.nix
     ./homeStylix.nix
+    ./home/blueTheme/swaync/swaync.nix
   ];
   # File management
   home.file = {
-    "/run/current-system/sw/etc/xdg/swaync/config.json".source = ./home/blueTheme/swaync/config.json;
-    "/run/current-system/sw/etc/xdg/swaync/style.css".source = ./home/blueTheme/swaync/style.css;
+        # "/run/current-system/sw/etc/xdg/swaync/config.json".source = ./home/blueTheme/swaync/config.json;
+        #"/run/current-system/sw/etc/xdg/swaync/style.css".source = ./home/blueTheme/swaync/style.css;
     #"/home/${username}/.config/hypr/" = { 
     #source = ./home/blueTheme/hypr; 
     #recursive = true; 
     #};
-    "/home/${username}/.config/swaync/" = {
-      source = ./home/blueTheme/swaync;
-      recursive = true;
-    };
+            # "/home/${username}/.config/swaync/" = {
+            #source = ./home/blueTheme/swaync;
+            #recursive = true;
+        #};
     "/home/${username}/.config/nvim/" = {
       source = ./home/blueTheme/nvim;
       recursive = true;
