929b9d40a4fcfe166e86ad2189a66fd5116c7c3d
bowyn Rosch <deathraymind@gmail.com>
2025-03-20 08:13:18 +0900

    adding swaync ssettings

--- hosts/default/home/blueTheme/swaync/swaync.nix
+++ hosts/default/home/blueTheme/swaync/swaync.nix
@@ -1,10 +1,17 @@
 {
-services.swaync.enable = true;
-
-
-
-
-
-
-
+    services.swaync = {
+        enable = true;
+        settings = {
+            positionX = "right";
+            positionY = "top";
+            control-center-margin-top = 10;
+            control-center-margin-bottom = 10;
+            control-center-margin-right = 10;
+            notification-icon-size = 64;
+            control-center-width = 500;
+            control-center-hight = 800;
+            notification-body-image-height = 100;
+            notification-body-image-width = 200;
+        };
+    };
 }
