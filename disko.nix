bad4d406ebb3bb8732c1896d2ee679f69843ac0d
Deathraymind <deathraymind@gmail.com>
2025-03-28 13:07:30 +0900

    disko seperate flake file

---
+++ disko.nix
@@ -0,0 +1,46 @@
+let
+    disk = builtins.getEnv "BOWOS_DISK"; # Without /dev/
+    swapSize = builtins.getEnv "BOWOS_SWAPSIZE"; # Just the size in gigs like 4 is = 4G
+
+in
+
+{
+   
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/${disk}"; # More portabl e
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              size = "${swapSize}G";
+              content = {
+                type = "swap";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
--- flake.nix
+++ flake.nix
@@ -53,43 +53,6 @@
 
             {
               networking.hostName = "bowos";
-              disko.devices = {
-          disk = {
-            main = {
-              # When using disko-install, we will overwrite this value from the commandline
-              device = "/dev/disk/by-id/some-disk-id";
-              type = "disk";
-              content = {
-                type = "gpt";
-                partitions = {
-                  MBR = {
-                    type = "EF02"; # for grub MBR
-                    size = "1M";
-                    priority = 1; # Needs to be first partition
-                  };
-                  ESP = {
-                    type = "EF00";
-                    size = "500M";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                      mountOptions = [ "umask=0077" ];
-                    };
-                  };
-                  root = {
-                    size = "100%";
-                    content = {
-                      type = "filesystem";
-                      format = "ext4";
-                      mountpoint = "/";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
             }
             home-manager.nixosModules.home-manager
             {
@@ -109,6 +72,7 @@
             stylix.nixosModules.stylix
             ./hosts/default/configuration.nix
             disko.nixosModules.disko
+            .disko.nix
             {
               networking.hostName = "bowos";
             }
