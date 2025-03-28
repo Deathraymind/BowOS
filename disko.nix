d337abc493b593a11bb64078a5a6a5f7c3621af7
Deathraymind <deathraymind@gmail.com>
2025-03-28 20:09:47 +0900

    disko

--- disko.nix
+++ disko.nix
@@ -1,16 +1,13 @@
 let
     disk = builtins.getEnv "BOWOS_DISK"; # Without /dev/
     swapSize = builtins.getEnv "BOWOS_SWAPSIZE"; # Just the size in gigs like 4 is = 4G
-
 in
-
 {
-   
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/some-disk-id";# More portabl e
+        device = "/dev/disk/by-id/some-disk-id";# More portable
         content = {
           type = "gpt";
           partitions = {
@@ -33,7 +30,7 @@ in
               size = "100%";
               content = {
                 type = "filesystem";
-                format = "tmpfs";
+                format = "ext4";  # Changed from tmpfs to ext4
                 mountpoint = "/";
               };
             };
@@ -43,4 +40,3 @@ in
     };
   };
 }
-
