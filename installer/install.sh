a38233772fad95859ba89db9f7c8b74f35c8d57b
Deathraymind <deathraymind@gmail.com>
2025-03-29 15:12:04 +0900

    testy

--- installer/install.sh
+++ installer/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Purpose: This is a terminal script installer for BowOS, an alternative to the Rust GUI installer.
-echo "System drives:"
-lsblk
 
 echo "What drive do you want to install on? (Enter the name only, e.g., for /dev/sda, just type sda)"
 read BOWOS_DRIVE
-
 
 echo "Is this system EFI or BIOS? (Type 'efi' or 'bios')"
 read BOOT_TYPE
@@ -19,6 +16,8 @@ fi
 echo "What do you want the username to be?"
 read BOWOS_USER
 
+echo "System drives:"
+lsblk
 
 echo "What swap size do you want? (Recommended: equal to your RAM in GB. Default is 4GB)"
 read -p "Enter swap size (in GB, e.g., 4): " BOWOS_SWAPSIZE
@@ -29,9 +28,11 @@ git clone https://github.com/deathraymind/bowos
 cd bowos/installer || exit
 
 if [[ "$BOOT_TYPE" == "bios" ]]; then
+    BOWOS_SWAPSIZE="$BOWOS_SWAPSIZE" BOWOS_USER="$BOWOS_USER" BOWOS_DISK="$BOWOS_DRIVE" \
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
         github:nix-community/disko -- --mode disko disko-bios.nix --arg device "/dev/$BOWOS_DRIVE"
 elif [[ "$BOOT_TYPE" == "efi" ]]; then
+    BOWOS_SWAPSIZE="$BOWOS_SWAPSIZE" BOWOS_USER="$BOWOS_USER" BOWOS_DISK="$BOWOS_DRIVE" \
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
         github:nix-community/disko -- --mode disko disko-uefi.nix --arg device "/dev/$BOWOS_DRIVE"
 else
@@ -47,7 +48,6 @@ else
     BOOT_DRIVE="$BOWOS_DRIVE"
     sudo -E bash bios-disk.sh
 fi
-
 
 cd ..
 sudo -E nixos-install --flake .#amd --no-root-passwd --impure
