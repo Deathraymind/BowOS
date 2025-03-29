20485e3ba038c2f7a73a35d159f547982e65189f
Deathraymind <deathraymind@gmail.com>
2025-03-29 12:57:40 +0900

    usage

--- disko-bios.nix
+++ disko-bios.nix
@@ -1,8 +1,12 @@
 let
   disk = builtins.getEnv "BOWOS_DISK"; # Without /dev/
   swapSize = builtins.getEnv "BOWOS_SWAPSIZE"; # Just the size in gigs like 4 is = 4G
-  # Usage:
-  # BOWOS_SWAPSIZE=4 BOWOS_USER=bowyn BOWOS_DISK=vda sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes github:nix-community/disko -- --mode disko disko-bios.nix
+ # run this command 
+    # BOWOS_SWAPSIZE=4 BOWOS_USER=bowyn BOWOS_DISK=vda sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes github:nix-community/disko -- --mode disko disko.nix --arg device /dev/vda
+    # nixos-generate-config --root /mnt
+    # nixos-install
+    # BOWOS_USER=bowyn sudo -E nixos-install --flake .#amd --no-root-passwd --impure 
+
 in
 {
   disko.devices = {
