f417a59096af735171201feafe798768a02a4ffa
Deathraymind <deathraymind@gmail.com>
2025-03-29 15:05:36 +0900

    installer fixv1.1.2

--- installer/install.sh
+++ installer/install.sh
@@ -3,7 +3,7 @@
 
 echo "What drive do you want to install on? (Enter the name only, e.g., for /dev/sda, just type sda)"
 read BOWOS_DRIVE
-BOWOS_DISK="/dev/$BOWOS_DRIVE"
+
 
 echo "Is this system EFI or BIOS? (Type 'efi' or 'bios')"
 read BOOT_TYPE
@@ -11,7 +11,7 @@ read BOOT_TYPE
 if [[ "$BOOT_TYPE" == "efi" ]]; then
     BOOT_DRIVE="nodev"
 else
-    BOOT_DRIVE="$BOWOS_DISK"
+    BOOT_DRIVE="$BOWOS_DRIVE"
 fi
 
 echo "What do you want the username to be?"
@@ -30,10 +30,10 @@ cd bowos/installer || exit
 
 if [[ "$BOOT_TYPE" == "bios" ]]; then
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
-        github:nix-community/disko -- --mode disko disko-bios.nix --arg device "$BOWOS_DISK"
+        github:nix-community/disko -- --mode disko disko-bios.nix --arg device "/dev/$BOWOS_DRIVE"
 elif [[ "$BOOT_TYPE" == "efi" ]]; then
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
-        github:nix-community/disko -- --mode disko disko-uefi.nix --arg device "$BOWOS_DISK"
+        github:nix-community/disko -- --mode disko disko-uefi.nix --arg device "/dev/$BOWOS_DRIVE"
 else
     echo "Invalid boot type. Exiting."
     exit 1
@@ -44,7 +44,7 @@ nixos-generate-config --root /mnt
 if [[ "$BOOT_TYPE" == "efi" ]]; then
     BOOT_DRIVE="nodev"
 else
-    BOOT_DRIVE="$BOWOS_DISK"
+    BOOT_DRIVE="$BOWOS_DRIVE"
     sudo -E bash bios-disk.sh
 fi
 
