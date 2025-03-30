7fef6aaeb017932aeebd7a312d5717ccde0f5d19
Deathraymind <deathraymind@gmail.com>
2025-03-30 11:52:27 +0900

    comments

--- installer/install.sh
+++ installer/install.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
-# Purpose: This is a terminal script installer for BowOS, an alternative to the Rust GUI installer.
+# Purpose: Terminal installer for BowOS – an alternative to the Rust GUI installer.
+
+# Display available system drives.
 echo "System drives:"
 lsblk
+
+# Ask for the target drive name (e.g. "sda" for /dev/sda)
 echo "What drive do you want to install on? (Enter the name only, e.g., for /dev/sda, just type sda)"
 read BOWOS_DRIVE
 
+# Ask whether the system boots using EFI or BIOS.
 echo "Is this system EFI or BIOS? (Type 'efi' or 'bios')"
 read BOOT_TYPE
 
+# Set BOOT_DRIVE variable based on boot type:
+# If EFI, use "nodev" (since EFI doesn't use a specific boot device for GRUB in this setup)
+# Otherwise (BIOS), use the drive specified.
 if [[ "$BOOT_TYPE" == "efi" ]]; then
     BOOT_DRIVE="nodev"
 else
     BOOT_DRIVE="$BOWOS_DRIVE"
 fi
 
+# Get the desired username and password for the new system.
 echo "What do you want the username to be?"
 read BOWOS_USER
 
 echo "What do you want the password to be?"
 read BOWOS_PASSWORD
 
-
-
+# Ask for the swap size (in GB). Defaults to 4GB if the user enters nothing.
 echo "What swap size do you want? (Recommended: equal to your RAM in GB. Default is 4GB)"
 read -p "Enter swap size (in GB, e.g., 4): " BOWOS_SWAPSIZE
 BOWOS_SWAPSIZE=${BOWOS_SWAPSIZE:-4} # Default to 4 if input is empty
 
-# Start installation process
+# Clone the BowOS repository and move into the installer directory.
 git clone https://github.com/deathraymind/bowos 
 cd bowos/installer || exit
 
+# Depending on the boot type (BIOS vs EFI), run the corresponding Disko command.
 if [[ "$BOOT_TYPE" == "bios" ]]; then
+    # For BIOS: Pass environment variables and run the disko command using the bios config.
     BOWOS_SWAPSIZE="$BOWOS_SWAPSIZE" BOWOS_USER="$BOWOS_USER" BOWOS_DISK="$BOWOS_DRIVE" \
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
         github:nix-community/disko -- --mode disko disko-bios.nix --arg device "/dev/$BOWOS_DRIVE"
 elif [[ "$BOOT_TYPE" == "efi" ]]; then
+    # For EFI: Pass the variables and run the disko command using the UEFI config.
     BOWOS_SWAPSIZE="$BOWOS_SWAPSIZE" BOWOS_USER="$BOWOS_USER" BOWOS_DISK="$BOWOS_DRIVE" \
     sudo -E nix run --extra-experimental-features nix-command --extra-experimental-features flakes \
         github:nix-community/disko -- --mode disko disko-uefi.nix --arg device "/dev/$BOWOS_DRIVE"
@@ -43,46 +54,47 @@ else
     exit 1
 fi
 
+# Remove the current /etc/nixos directory (to prevent conflicts with previous configs).
 rm -r /etc/nixos 
 
+# Generate new NixOS configuration for the target system.
 nixos-generate-config --root /mnt
+
+# Copy the auto-generated NixOS configuration from the mounted partition into /etc/nixos.
 cp -r /mnt/etc/nixos/* /etc/nixos
-# Copy preferences directory 
+
+# Copy the BowOS preferences directory into /etc/nixos (this holds custom configuration files).
 cp -r preferences /etc/nixos
 
-# Update grub configuration based on boot type
+# Update the GRUB configuration based on the boot type.
 if [ "$BOOT_DRIVE" == "nodev" ]; then
-    # For initial install
+    # For EFI systems, copy preferences into both /etc/nixos and /mnt/etc/nixos.
     cp -r preferences /etc/nixos
     cp -r preferences /mnt/etc/nixos
-
-    # For constant preference
-
 else
-    # For initial install
+    # For BIOS systems, remove any existing GRUB device/efiSupport settings,
+    # then insert the correct GRUB device configuration into the generated config.
     sed -i '/boot.loader.grub.device/d' /mnt/etc/nixos/configuration.nix
     sed -i '/boot.loader.grub.efiSupport/d' /mnt/etc/nixos/configuration.nix
     sed -i "20i boot.loader.grub.device = \"/dev/$BOOT_DRIVE\";" /mnt/etc/nixos/configuration.nix
 
-
-    # For constant preference
+    # Also update the preferences file with the correct boot device.
     sed -i '/boot.loader.grub.device/d' /mnt/etc/nixos/preferences/configuration-preferences.nix
     sed -i "5i boot.loader.grub.device = \"/dev/$BOOT_DRIVE\";" /mnt/etc/nixos/preferences/configuration-preferences.nix
     sed -i '/boot.loader.grub.efiSupport/d' /mnt/etc/nixos/configuration.nix
-
 fi
 
-# Install NixOS
+# Install NixOS using your flake configuration.
+# --no-root-passwd indicates that no root password is being set during install.
 BOWOS_USER="$BOWOS_USER" sudo -E nixos-install --flake .#amd --no-root-passwd --impure
 
-
+# Enter the new system to set the user and root passwords.
+# This uses nixos-enter and an expect script (inside a nix-shell) to automate password setting.
 BOWOS_USER="$BOWOS_USER" BOWOS_PASSWORD="$BOWOS_PASSWORD" nixos-enter -- nix-shell -p expect --extra-experimental-features flakes --run '
-  # Set the username and password
-
-  # Create the user
+  # Create the new user account.
   useradd -m "$BOWOS_USER"
 
-  # Set the password for the new user using expect
+  # Use expect to set the password for the new user.
   expect -c "
     spawn passwd $BOWOS_USER
     expect \"New password:\"
@@ -92,7 +104,7 @@ BOWOS_USER="$BOWOS_USER" BOWOS_PASSWORD="$BOWOS_PASSWORD" nixos-enter -- nix-she
     expect eof
   "
 
-  # Set the password for root using expect
+  # Use expect to set the root password.
   expect -c "
     spawn passwd root
     expect \"New password:\"
@@ -101,5 +113,5 @@ BOWOS_USER="$BOWOS_USER" BOWOS_PASSWORD="$BOWOS_PASSWORD" nixos-enter -- nix-she
     send \"$BOWOS_PASSWORD\r\"
     expect eof
   "
-  '
+'
 
