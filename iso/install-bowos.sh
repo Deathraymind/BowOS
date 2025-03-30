bd2545c7278c08d3e9a352e63dae2af39b34fe56
Deathraymind <deathraymind@gmail.com>
2025-03-30 20:18:13 +0900

    remove iso

--- iso/install-bowos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure we are running as root
-rm -r BowOS
-git clone --branch nvidia-fix https://github.com/deathraymind/BowOS 
-cd BowOS 
-nix-shell --run '
-python3 disk_formatter.py
-'
